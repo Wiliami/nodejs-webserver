2daf9f937690cfcc4b7cda2f8530fda47a779890
wiliami <wiliaup97@gmail.com>
2024-04-15 16:13:08 -0400

    add implemention math functions

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+const sum = require('./math/sum');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,10 +11,7 @@ const server = http.createServer((req, res) => {
     res.end('Hello World\n');
 });
 
-sum();
-subtraction();
-multiplication();
-division();
+sum(1, 1);
 
 
 server.listen(port, hostname, () => { 
