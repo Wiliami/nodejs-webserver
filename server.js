df5e905ab56de4d5c3083f46abddbd699537e86b
wiliami <wiliaup97@gmail.com>
2024-05-07 15:18:02 -0400

    add feature example create server lessframework

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const http = require('node:http');
-const sum = require('./math/sum');
-const division = require('./math/division');
-
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
 });
-
-sum(1, 1);
-division(1, 1);
 
 server.listen(port, hostname, () => { 
     console.log(`Server running at http://${hostname}:${port}/`)
